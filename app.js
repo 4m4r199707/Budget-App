bbf6b9699a57e691c3a64dd8cf283f2aa6f29ea9
4m4r199707 <34017886+4m4r199707@users.noreply.github.com>
2019-08-06 18:44:33 +0530

    updating controller
    null values are checked if empty does'nt work

--- app.js
+++ app.js
@@ -79,7 +79,7 @@ var UIController = (function() {
             return {
                 type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
                 description: document.querySelector(DOMstrings.inputDescription).value,
-                value: document.querySelector(DOMstrings.inputValue).value,
+                value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
         },
 
@@ -148,22 +148,33 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
     };
 
+    var updateBudget = function() {
+        //1. Calculate the budget
+
+        //2. return the budget
+
+        // 3. Display the budget on the UI
+    }
+
+
     var ctrlAddItem = function() {
         // To do list 1. to get the input data
         var input, newItem;
         input = UICtrl.getInput();
 
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+        if (input.description !== "" && !isNaN(input.value) && input.value > 0) {
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
-        //3. Add the item to the UI
-        UICtrl.addListItem(newItem, input.type);
+            //3. Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
 
-        //4. clear the fields
-        UICtrl.clearFields();
-        //5. Calculate the budget
+            //4. clear the fields
+            UICtrl.clearFields();
 
-        // 6. Display the budget on the UI
+            //5. calculate and update budget 
+            updateBudget();
+        }
 
     };
 
