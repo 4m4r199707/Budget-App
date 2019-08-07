583f77665f9a63e8edd15fccc3309c961bfb1374
4m4r199707 <34017886+4m4r199707@users.noreply.github.com>
2019-08-08 02:05:16 +0530

    UI small change
    RED -  BLUE

--- app.js
+++ app.js
@@ -197,6 +197,16 @@ var UIController = (function() {
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
     };
 
+
+
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+
+
+
     return {
         getInput: function() {
             return {
@@ -261,11 +271,7 @@ var UIController = (function() {
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
+
 
             nodeListForEach(fields, function(current, index) {
                 if (percentages[index] > 0) {
@@ -284,6 +290,18 @@ var UIController = (function() {
             month = now.getMonth();
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + " " + year;
 
+        },
+
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' + DOMstrings.inputDescription +
+                ',' + DOMstrings.inputValue);
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+            document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
         },
         getDOMstrings: function() {
             return DOMstrings;
@@ -328,6 +346,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         });
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType)
     };
 
     var updateBudget = function() {
